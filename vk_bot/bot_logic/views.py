32c4fd19e9fe02978af14419723a19208232334c
pavlove <pavlove75@mail.ru>
2022-09-18 20:04:14 +0300

    настройка ответных сообщений

--- vk_bot/bot_logic/views.py
+++ vk_bot/bot_logic/views.py
@@ -3,15 +3,23 @@ from django.http import HttpResponse
 from django.conf import settings
 import json
 
-from bot_logic.vk_bot_logic import send_message, section_dict, button_response
+from bot_logic.vk_bot_logic import send_message, section_dict, button_response, vk_session, vk, vk_upload
 
 
 @csrf_exempt
 def index(request):
-	body = json.loads(request.body)
-	if body == settings.VK_BODY_DATA:
-		return HttpResponse(settings.VK_GET_KEY)
-	elif body['type'] == 'message_new':
-		msg = body['object']['message']['text']
-		user_id = body['object']['message']['from_id']
-		send_message(event=body, message=f'Какой-то текст {msg}, {user_id}')
+    if request.method == "POST":
+        data = json.loads(request.body.decode('utf-8'))  # take POST request from auto-generated variable <request.body> in json format
+        if data['type'] == 'confirmation':# if VK server request confirmation
+            return settings.VK_GET_KEY
+        elif data['type'] == 'message_new':# if VK server send a message
+            from_id = data['object']['message']['from_id']
+            vk.messages.send(
+                message=f'Hello World! {str(data)}',
+                random_id=0,
+                peer_id=from_id
+            )
+            send_message(event=data['object']['message'], message='Ну вот ещё')
+            return HttpResponse('ok', content_type="text/plain", status=200)
+    else:
+        return HttpResponse('see you :)')
--- vk_bot/bot_logic/vk_bot_logic.py
+++ vk_bot/bot_logic/vk_bot_logic.py
@@ -2,14 +2,14 @@ import vk_api
 from vk_api import VkUpload
 from vk_api.keyboard import VkKeyboard
 from django.conf import settings
-
+from products.models import Category, Product
 
 vk_session = vk_api.VkApi(token=settings.VK_TOKEN)
 vk = vk_session.get_api()
 vk_upload = VkUpload(vk)
 
 keyboard = VkKeyboard(one_time=False, inline=True)
-section_dict = {'1': 1, '2': 2}
+section_dict = {i.category_name: i.id for i in Category.objects.filter(activity=True)}
 for elem, value in section_dict.items():
 	keyboard.add_button(elem)
 
@@ -23,19 +23,20 @@ def send_message(**kwargs):
 	"""
 	post = {
 		'keyboard': keyboard.get_keyboard(),
-		'random_id': 0
+		'random_id': 0,
+		'peer_id': kwargs['event']['from_id'],
+		'message': kwargs['message']
 	}
-	for element in kwargs:
-		if element == 'event':
-			if kwargs['event'].from_user:
-				post['user_id'] = kwargs['event'].user_id
-			elif kwargs['event'].from_chat:
-				post['chat_id'] = kwargs['event'].chat_id
-		elif element == 'keyboard':
-			if kwargs['keyboard'] is False:
-				post.pop('keyboard')
-		else:
-			post[element] = kwargs[element]
+	for element in kwargs['event']:
+		if element == "group_id":
+			post['chat_id'] = element["group_id"]
+# 			elif kwargs['event'].from_chat:
+# 				post['chat_id'] = kwargs['event'].chat_id
+#  		elif element == 'keyboard':
+# 			if kwargs['keyboard'] is False:
+# 				post.pop('keyboard')
+# 		else:
+# 		post[element] = kwargs[element]
 	vk.messages.send(**post)
 
 
